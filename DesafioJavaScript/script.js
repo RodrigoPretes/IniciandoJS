f25ba8e4f5bfcf54231de4138a6281e12b5c081b
Rodrigo Pretes <85810503+RodrigoPretes@users.noreply.github.com>
2023-05-15 14:47:49 -0300

    Add files via upload

---
+++ DesafioJavaScript/script.js
@@ -0,0 +1,86 @@
+// 1 - Crie um script que exiba a mensagem "Hello World!" em um alerta no navegador.
+
+  /*alert("Hello World");*/
+
+
+// 2 - Crie um script que declare duas variáveis e exiba o resultado da soma entre elas.
+
+/*let a = 2;
+let b = 3;
+  alert(a+b);*/
+
+// Crie um script que declare uma variável e verifique se o seu valor é um número. 
+//Se for, exiba a mensagem "É um número", caso contrário, 
+//exiba a mensagem "Não é um número".
+
+/*let a = 50;
+
+const VerifyNumber = typeof a == "number" ? "É um número" : "Não é um número";
+
+
+alert(VerifyNumber);*/
+
+//Crie um script que declare uma variável e verifique se o seu valor é uma string. 
+//Se for, exiba a mensagem "É uma string", caso contrário,
+// exiba a mensagem "Não é uma string".
+
+/*let a = "abc";
+
+const VerifyString = typeof a == "string" ? "É uma string" : "Não é uma string";
+
+
+alert(VerifyString);*/
+
+//Crie um script que declare uma variável e verifique se o seu valor é um booleano. 
+//Se for, exiba a mensagem "É um booleano", caso contrário,
+// exiba a mensagem "Não é um booleano".
+
+/*let a = false;
+
+const VerifyBoolean = typeof a == "boolean" ? "É um booleano" : "Não é um booleano";
+
+
+alert(VerifyBoolean);*/
+
+//Crie um script que declare duas variáveis e exiba o resultado da subtração entre elas.
+
+  /*let a = 5;
+  let b = 6;
+
+  alert(a-b);*/
+
+//Crie um script que declare duas variáveis e exiba o resultado da multiplicação entre elas.
+
+ /* let a = 5;
+  let b = 6;
+
+  alert(a*b);*/
+
+//Crie um script que declare duas variáveis e exiba o resultado da divisão entre elas.
+
+  /*let a = 5;
+  let b = 6;
+
+  alert(a/b);*/
+
+//Crie um script que declare uma variável e verifique se o seu valor é um número par.
+ //Se for, exiba a mensagem "É um número par", 
+ //caso contrário, exiba a mensagem "Não é um número par".
+
+ /*let a = 7;
+
+  const VerifyBy = a %2==0 ? "É um número par" : "Não é um número par";
+
+
+  alert(VerifyBy);*/
+
+//Crie um script que declare uma variável e verifique se o seu valor é um número ímpar.
+//Se for, exiba a mensagem "É um número ímpar", caso contrário,
+//exiba a mensagem "Não é um número ímpar".
+
+/*let a = 10;
+
+const VerifyOdd = a %2!=0 ? "É um número ímpar" : "Não é um número ímpar";
+
+
+alert(VerifyOdd);*/
